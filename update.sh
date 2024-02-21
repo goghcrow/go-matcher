6675288f41677b41b15b06f13c19a049e255cd5a
xiao <>
2024-02-21 19:23:41 +0800

    add update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ./../go-ast-matcher
+./update.sh
+git commit -am "update go.mod" && git push origin
+cd -
+
+cd ./../go-callgraph
+./update.sh
+git commit -am "update go.mod" && git push origin
+cd -
